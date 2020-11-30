9eeb328ddbe06168246ec6958eab0ae0f12d5087
Ayabulela Mahlathini <atrad75@gmail.com>
2020-11-30 18:29:31 +0200

    deploy

--- src/main/java/com/digital/epharmacy/ePharmacyDeliverySystemApplication.java
+++ src/main/java/com/digital/epharmacy/ePharmacyDeliverySystemApplication.java
@@ -1,9 +1,9 @@
 package com.digital.epharmacy;
 //Author: Nicole Hawthorne- Main Springboot application
 
+import com.digital.epharmacy.config.MyIntercepter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import digital.epharmacy.pharmacy.config.MyIntercepter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,19 +17,19 @@ public class ePharmacyDeliverySystemApplication {
         SpringApplication.run(ePharmacyDeliverySystemApplication.class, args);
     }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedOrigin("https://vibekonnect.tech/admin-dashboard");
-        config.addAllowedOrigin("https://amahlathini.github.io/admin-dashboard");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedOrigin("http://localhost:3000");
+//        config.addAllowedOrigin("https://vibekonnect.tech/admin-dashboard");
+//        config.addAllowedOrigin("https://amahlathini.github.io/admin-dashboard");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -37,7 +37,9 @@ public class ePharmacyDeliverySystemApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "http://localhost:3000", "https://epharmacy-sa.herokuapp.com", "https://amahlathini.github.io/admin-dashboard", "https://vibekonnect.tech/admin-dashboard");
+                        .allowedOrigins("http://localhost:4200", "http://localhost:3000", "https://epharmacy-sa.herokuapp.com", "https://amahlathini.github.io/admin-dashboard", "https://vibekonnect.tech/admin-dashboard")
+                        .allowedHeaders("*")
+                        .allowedMethods("*");
             }
         };
     }
