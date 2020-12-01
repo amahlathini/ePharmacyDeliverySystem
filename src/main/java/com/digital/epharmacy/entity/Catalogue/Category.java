fb6ee6184598afa37f1d15f4944e64f0a86ce52f
Ayabulela Mahlathini <atrad75@gmail.com>
2020-12-01 11:56:37 +0200

    deploy

--- src/main/java/com/digital/epharmacy/entity/Catalogue/Category.java
+++ src/main/java/com/digital/epharmacy/entity/Catalogue/Category.java
@@ -13,7 +13,7 @@ public class Category implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    //@Column(name = "id", insertable = false, updatable = false)
     private long id;
     @NotNull
     private String category_name;
